8396016f7d59bb380d31be789926b960461135a3
Denis <korbakovd@gmail.com>
2026-01-06 12:46:15 +0300

    fix: update migration

--- db/migrations/001_create_users_table.sql
+++ db/migrations/001_create_users_table.sql
@@ -4,6 +4,7 @@ CREATE TABLE users
     id         UUID PRIMARY KEY,
     name       VARCHAR(255) NOT NULL,
     email      VARCHAR(255) NOT NULL UNIQUE,
+    avatar     VARCHAR(255) NOT NULL,
     created_at TIMESTAMP,
     updated_at TIMESTAMP
 );
