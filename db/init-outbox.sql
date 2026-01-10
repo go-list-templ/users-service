f19c058a4a4df77832309646200fb9d47feb1fd4
Denis <korbakovd@gmail.com>
2026-01-10 23:26:37 +0300

    fix: put logic for create outbox in new file

---
+++ db/init-outbox.sql
@@ -0,0 +1,12 @@
+create table outbox
+(
+    message_id uuid primary key,
+    message    jsonb
+);
+
+alter table outbox
+    replica identity full;
+
+create publication sequin_pub for table outbox;
+
+select pg_create_logical_replication_slot('sequin_slot', 'pgoutput');
