9af43d69ebf92563a6751adec2708d673f99905e
Denis <korbakovd@gmail.com>
2026-01-15 21:05:53 +0300

    fix: update register logic

--- internal/controller/grpc/router.go
+++ internal/controller/grpc/router.go
@@ -1,16 +1,18 @@
 package grpc
 
 import (
-	v1 "github.com/go-list-templ/grpc/internal/controller/grpc/v1"
-	"github.com/go-list-templ/grpc/internal/usecase"
+	"github.com/go-list-templ/grpc/internal/controller/grpc/v1/user"
+	"github.com/go-list-templ/grpc/internal/usecase/user/command"
+	"github.com/go-list-templ/grpc/internal/usecase/user/query"
 	"go.uber.org/zap"
 	pbgrpc "google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
 
-func NewRouter(app *pbgrpc.Server, u *usecase.User, l *zap.Logger) {
+func NewRouter(app *pbgrpc.Server, uq *query.UserUsecase, uc *command.UserUsecase, l *zap.Logger) {
 	{
-		v1.NewUserRoute(app, u, l)
+		user.NewQueryService(app, uq, l)
+		user.NewCommandService(app, uc, l)
 	}
 
 	reflection.Register(app)
