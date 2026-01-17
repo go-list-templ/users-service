5369b093aefa81fe6680df73c11d464156fe0323
Denis <korbakovd@gmail.com>
2026-01-17 17:12:12 +0300

    feat: add transaction manager for create user

--- internal/usecase/user/command/usecase.go
+++ internal/usecase/user/command/usecase.go
@@ -6,26 +6,31 @@ import (
 	"github.com/go-list-templ/grpc/internal/domain/entity"
 	"github.com/go-list-templ/grpc/internal/domain/event"
 	"github.com/go-list-templ/grpc/internal/repo"
+	"github.com/go-list-templ/grpc/pkg/trm"
 )
 
 type UserUsecase struct {
 	userRepo   repo.UserRepo
 	outboxRepo repo.OutboxRepo
+
+	trm *trm.Manager
 }
 
-func NewUserUsecase(u repo.UserRepo, o repo.OutboxRepo) *UserUsecase {
-	return &UserUsecase{userRepo: u, outboxRepo: o}
+func NewUserUsecase(u repo.UserRepo, o repo.OutboxRepo, t *trm.Manager) *UserUsecase {
+	return &UserUsecase{u, o, t}
 }
 
 func (u *UserUsecase) Create(ctx context.Context, user entity.User) (entity.User, error) {
-	err := u.userRepo.Store(ctx, user)
-	if err != nil {
-		return entity.User{}, err
-	}
+	err := u.trm.Do(ctx, func(ctx context.Context) error {
+		err := u.userRepo.Store(ctx, user)
+		if err != nil {
+			return err
+		}
 
-	userCreated := event.NewUserCreated(user)
+		userCreated := event.NewUserCreated(user)
 
-	err = u.outboxRepo.Publish(ctx, userCreated.Event)
+		return u.outboxRepo.Publish(ctx, userCreated.Event)
+	})
 	if err != nil {
 		return entity.User{}, err
 	}
