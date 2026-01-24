a6466ec8c2ff6efe23e159d7650b59c062ac7b14
Denis <korbakovd@gmail.com>
2026-01-24 13:42:19 +0300

    feat: add dao for entity user

---
+++ internal/domain/event/dao/user.go
@@ -0,0 +1,16 @@
+package dao
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID        uuid.UUID `db:"id"`
+	Name      string    `db:"name"`
+	Email     string    `db:"email"`
+	Avatar    string    `db:"avatar"`
+	CreatedAt time.Time `db:"created_at"`
+	UpdatedAt time.Time `db:"updated_at"`
+}
