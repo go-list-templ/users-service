ad45735940c15dc387662aae96073bfa3c022558
Denis <korbakovd@gmail.com>
2026-01-24 13:35:50 +0300

    fix: add old logic router

--- internal/controller/grpc/router.go
+++ internal/controller/grpc/router.go
@@ -1,29 +1,18 @@
 package grpc
 
 import (
+	v1 "github.com/go-list-templ/grpc/internal/controller/grpc/v1"
 	pbgrpc "google.golang.org/grpc"
 
-	"github.com/go-list-templ/grpc/internal/controller/grpc/v1/user"
-	"github.com/go-list-templ/grpc/internal/usecase/user/command"
-	"github.com/go-list-templ/grpc/internal/usecase/user/query"
+	"github.com/go-list-templ/grpc/internal/usecase/user"
 	"go.uber.org/zap"
 	"google.golang.org/grpc/reflection"
 )
 
-type Router struct {
-	server *pbgrpc.Server
-	logger *zap.Logger
-}
+func NewRouter(s *pbgrpc.Server, u *user.Usecase, l *zap.Logger) {
+	{
+		v1.NewUserRoute(s, u, l)
+	}
 
-func NewRouter(s *pbgrpc.Server, l *zap.Logger) *Router {
-	return &Router{server: s, logger: l}
-}
-
-func (r *Router) Release() {
-	reflection.Register(r.server)
-}
-
-func (r *Router) User(uq *query.UserUsecase, uc *command.UserUsecase) {
-	user.NewQueryService(r.server, uq, r.logger)
-	user.NewCommandService(r.server, uc, r.logger)
+	reflection.Register(s)
 }
