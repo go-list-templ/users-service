24f955c26d012fd1989d5d8ebeee15260aa7a5de
Denis <korbakovd@gmail.com>
2026-01-24 13:36:31 +0300

    fix: add logic for marshal user

--- internal/domain/event/user_created.go
+++ internal/domain/event/user_created.go
@@ -1,6 +1,7 @@
 package event
 
 import (
+	"encoding/json"
 	"github.com/go-list-templ/grpc/internal/domain/entity"
 )
 
@@ -8,12 +9,17 @@ type UserCreated struct {
 	Event
 }
 
-func NewUserCreated(user entity.User) UserCreated {
+func NewUserCreated(user entity.User) (UserCreated, error) {
+	payload, err := json.Marshal(user)
+	if err != nil {
+		return UserCreated{}, err
+	}
+
 	return UserCreated{
 		*NewEvent(
 			user.ID.Value().String(),
-			"user",
-			nil,
+			"user_created",
+			payload,
 		),
-	}
+	}, nil
 }
