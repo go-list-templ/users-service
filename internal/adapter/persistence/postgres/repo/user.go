e64c831cd4d2e5be7add49236a2e5ebc52dc7573
Denis <korbakovd@gmail.com>
2026-01-30 20:08:41 +0300

    fix: add to scan all rows

--- internal/adapter/persistence/postgres/repo/user.go
+++ internal/adapter/persistence/postgres/repo/user.go
@@ -7,16 +7,18 @@ import (
 	"github.com/go-list-templ/grpc/internal/adapter/persistence/postgres/repo/dao"
 	"github.com/go-list-templ/grpc/internal/adapter/persistence/postgres/transaction"
 	"github.com/go-list-templ/grpc/internal/core/domain/entity"
+	"go.uber.org/zap"
 )
 
 type UserRepo struct {
 	*postgres.Postgres
 
+	logger *zap.Logger
 	getter *transaction.TrmGetter
 }
 
-func NewUserRepo(p *postgres.Postgres, g *transaction.TrmGetter) *UserRepo {
-	return &UserRepo{p, g}
+func NewUserRepo(p *postgres.Postgres, l *zap.Logger, g *transaction.TrmGetter) *UserRepo {
+	return &UserRepo{p, l, g}
 }
 
 func (u *UserRepo) Store(ctx context.Context, user entity.User) error {
@@ -43,6 +45,7 @@ func (u *UserRepo) All(ctx context.Context) ([]entity.User, error) {
 
 	rows, err := u.Query(ctx, "SELECT * FROM users")
 	if err != nil {
+		u.logger.Warn("failed query", zap.Error(err))
 		return users, err
 	}
 	defer rows.Close()
@@ -50,8 +53,16 @@ func (u *UserRepo) All(ctx context.Context) ([]entity.User, error) {
 	for rows.Next() {
 		var userDAO dao.User
 
-		err = rows.Scan(&userDAO)
+		err = rows.Scan(
+			&userDAO.ID,
+			&userDAO.Name,
+			&userDAO.Email,
+			&userDAO.Avatar,
+			&userDAO.CreatedAt,
+			&userDAO.UpdatedAt,
+		)
 		if err != nil {
+			u.logger.Warn("failed scan", zap.Error(err))
 			return nil, err
 		}
 
