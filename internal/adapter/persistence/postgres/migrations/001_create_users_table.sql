a710598777ee8548a3a31499fa6544cc11bef4bd
Denis <korbakovd@gmail.com>
2026-02-03 21:40:39 +0300

    fix: update comment for goose

--- internal/adapter/persistence/postgres/migrations/001_create_users_table.sql
+++ internal/adapter/persistence/postgres/migrations/001_create_users_table.sql
@@ -1,4 +1,4 @@
--- migrate:up
+-- +goose Up
 CREATE TABLE users
 (
     id         UUID PRIMARY KEY,
@@ -9,8 +9,5 @@ CREATE TABLE users
     updated_at TIMESTAMP
 );
 
-CREATE INDEX idx_users_email ON users (email);
-CREATE INDEX idx_users_created_at ON users (created_at);
-
--- migrate:down
+-- +goose Down
 DROP TABLE IF EXISTS users
