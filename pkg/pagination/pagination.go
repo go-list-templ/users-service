214be1e70809685c202c8a33ac109ec45a4d9aba
Denis <korbakovd@gmail.com>
2026-02-15 02:19:52 +0300

    feat: create struct for pagination

---
+++ pkg/pagination/pagination.go
@@ -0,0 +1,62 @@
+package pagination
+
+import (
+	"encoding/base64"
+	"errors"
+)
+
+const (
+	DefaultLimitList = 10
+	MaxLimitList     = 100
+)
+
+var (
+	ErrEmptyCursor   = errors.New("empty cursor")
+	ErrInvalidCursor = errors.New("invalid cursor")
+)
+
+type Paginate struct {
+	PageSize int64
+	Token    string
+}
+
+func New(pageSize int64, token string) *Paginate {
+	return &Paginate{PageSize: pageSize, Token: token}
+}
+
+func (p *Paginate) Limit() int {
+	limit := int(p.PageSize)
+
+	if limit <= 0 {
+		limit = DefaultLimitList
+	}
+
+	if limit >= MaxLimitList {
+		limit = MaxLimitList
+	}
+
+	return limit
+}
+
+func (p *Paginate) Cursor() string {
+	if p.Token == "" {
+		return ""
+	}
+
+	return base64.StdEncoding.EncodeToString([]byte(p.Token))
+}
+
+func (p *Paginate) GenerateToken(cursor string) (string, error) {
+	if cursor == "" {
+		return "", ErrEmptyCursor
+	}
+
+	genToken, _ := base64.StdEncoding.DecodeString(cursor)
+	token := string(genToken)
+
+	if p.Token == token {
+		return "", ErrInvalidCursor
+	}
+
+	return token, nil
+}
