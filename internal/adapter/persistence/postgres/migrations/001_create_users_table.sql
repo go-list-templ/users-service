1ec297f808bb81c0f505de8ec91eae3580464693
Denis <korbakovd@gmail.com>
2026-03-28 15:24:01 +0300

    fix: update fields

--- internal/adapter/persistence/postgres/migrations/001_create_users_table.sql
+++ internal/adapter/persistence/postgres/migrations/001_create_users_table.sql
@@ -2,7 +2,8 @@
 CREATE TABLE users
 (
     id         UUID PRIMARY KEY,
-    name       VARCHAR(255) NOT NULL,
+    name       VARCHAR(255) NULL,
+    password   VARCHAR(255) NOT NULL,
     email      VARCHAR(255) NOT NULL UNIQUE,
     avatar     VARCHAR(255) NOT NULL,
     created_at TIMESTAMP,
