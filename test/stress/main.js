4c4d4b1cb08aca5d4cecc6066a56888070df16d3
Denis <korbakovd@gmail.com>
2026-03-29 23:37:52 +0300

    fix: update integration test

--- test/stress/main.js
+++ test/stress/main.js
@@ -1,15 +1,16 @@
 import grpc from 'k6/net/grpc'
 import {check} from 'k6'
-import {create, list} from './grpc/user.js'
 import {healthz} from "./http/healthz.js"
+import {create, getByEmail, list, verifyCred} from "./grpc/user.js"
 
 const grpcUrl = 'app:8080'
 const diagnosticUrl = 'http://app:8081'
 
+const tokens = {};
+const usersCred = {name: "", password: "", email: ""};
+
 const client = new grpc.Client()
 client.load(['/src/proto/api/user/v1'], 'user.proto')
-
-const tokens = {};
 
 export const options = {
     scenarios: {
@@ -23,6 +24,28 @@ export const options = {
                 {target: 100, duration: '1m'},
             ],
             exec: 'runCreate',
+        },
+        verify_cred_user_grpc: {
+            executor: 'ramping-arrival-rate',
+            startRate: 5,
+            timeUnit: '1s',
+            preAllocatedVUs: 5,
+            maxVUs: 5,
+            stages: [
+                {target: 50, duration: '1m'},
+            ],
+            exec: 'runVerifyCred',
+        },
+        get_by_email_user_grpc: {
+            executor: 'ramping-arrival-rate',
+            startRate: 5,
+            timeUnit: '1s',
+            preAllocatedVUs: 5,
+            maxVUs: 5,
+            stages: [
+                {target: 50, duration: '1m'},
+            ],
+            exec: 'runGetByEmail',
         },
         list_users_grpc: {
             executor: 'ramping-arrival-rate',
@@ -38,17 +61,19 @@ export const options = {
         },
         healthz_http: {
             executor: 'constant-arrival-rate',
-            exec: 'runHealthz',
             rate: 2,
             timeUnit: '5s',
             duration: '1m',
             preAllocatedVUs: 1,
             maxVUs: 1,
+            exec: 'runHealthz',
         },
     },
     thresholds: {
         'grpc_req_duration{scenario:create_user_grpc}': ['p(95) < 100'],
         'grpc_req_duration{scenario:list_users_grpc}': ['p(95) < 100'],
+        'grpc_req_duration{scenario:verify_cred_user_grpc}': ['p(95) < 100'],
+        'grpc_req_duration{scenario:get_by_email_user_grpc}': ['p(95) < 100'],
         'http_req_duration{scenario:healthz_http}': ['p(95) < 500'],
         'checks': ['rate >= 0.9']
     },
@@ -56,11 +81,15 @@ export const options = {
 }
 
 export function runCreate() {
-    client.connect(grpcUrl, {plaintext: true})
+    const vu = __VU;
+    const iter = __ITER;
+
+    client.connect(grpcUrl, {plaintext: true});
 
     const payload = {
-        name: `user_${__ITER}`,
-        email: `mail${__VU}_${__ITER}@example.com`,
+        name: `user_${iter}`,
+        email: `mail${vu}_${iter}@example.com`,
+        password: "password"
     };
 
     const response = create(client, payload)
@@ -69,13 +98,18 @@ export function runCreate() {
         'create_user status is OK': (r) => r && r.status === grpc.StatusOK,
     })
 
-    client.close()
+    if (usersCred.name === "") {
+        usersCred.name = payload.name
+        usersCred.email = payload.email
+        usersCred.password = payload.password
+    }
 }
 
 export function runList() {
-    client.connect(grpcUrl, {plaintext: true})
-
     const vu = __VU;
+
+    client.connect(grpcUrl, {plaintext: true});
+
     const payload = {page_token: tokens[vu]};
 
     const response = list(client, payload)
@@ -87,8 +121,35 @@ export function runList() {
     if (response && response.status === grpc.StatusOK) {
         tokens[vu] = JSON.parse(JSON.stringify(response.message)).nextPageToken
     }
+}
 
-    client.close()
+export function runGetByEmail() {
+    client.connect(grpcUrl, {plaintext: true});
+
+    const payload = {
+        email: usersCred.email,
+    };
+
+    const response = getByEmail(client, payload)
+
+    check(response, {
+        'create_user status is OK': (r) => r && r.status === grpc.StatusOK,
+    })
+}
+
+export function runVerifyCred() {
+    client.connect(grpcUrl, {plaintext: true});
+
+    const payload = {
+        email: usersCred.email,
+        password: usersCred.password
+    };
+
+    const response = verifyCred(client, payload)
+
+    check(response, {
+        'create_user status is OK': (r) => r && r.status === grpc.StatusOK,
+    })
 }
 
 export function runHealthz() {
